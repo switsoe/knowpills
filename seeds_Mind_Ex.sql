4540d1faf5b0ccefa9621d39fa2c89b882b99216
davidlightman83 <switsoe@gmail.com>
2018-06-12 20:10:23 -0600

    added a schema, renamed schemas and seeds, corrected typos in both tables***one more missed typo, so use this commit

--- seeds_Mind_Ex.sql
+++ seeds_Mind_Ex.sql
@@ -8,7 +8,7 @@ INSERT INTO Mind_Ex (Name, Details)
 VALUES ("Can mindfulness help with addiction?", "Since mindfulness encourages people to pay attention to their thoughts, it can train them to recognize temptation. A certain thought, if left unchecked, can become a distraction and hard to ignore. Learning how to recognize the thought as the origin of drug-use, however, often helps people break this cycle. Through mindfulness, people in outpatient programs can stop thoughts from controlling their actions.");
 
 INSERT INTO Mind_Ex (Name, Details)
-VALUES ("How does one practice Mindfullness?", "People can practice mindfulness in a variety of ways. People who experience chronic pain, for instance, often use body scan meditation to identify areas of pain and live within the moment instead of projecting the pain into the future. 
+VALUES ("How does one practice mindfulness?", "People can practice mindfulness in a variety of ways. People who experience chronic pain, for instance, often use body scan meditation to identify areas of pain and live within the moment instead of projecting the pain into the future. 
 Other forms of mindfulness practice include; seated meditation that asks you to focus on your breathing, observing-thought meditation, where you acknowledge thoughts that arise in your mind and let them go without judgment, walking meditation that lets you pay careful attention to the details of your surroundings.");
 
 INSERT INTO Mind_Ex (Name, Details)
